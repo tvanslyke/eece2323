c22bbcd94a4f84548e06b43a4877f6af7fee5640
Tim VanSlyke <vanslyke.t@husky.neu.edu>
2018-02-10 14:19:57 -0500

    Turns out it makes more sense to just have outputs be registers.

--- lab3-report/verilog/eightbit_adder.v
+++ lab3-report/verilog/eightbit_adder.v
@@ -1,17 +1,12 @@
 module eightbit_palu(
 		input signed [7:0] a,
 		input signed [7:0] b,
-		output [7:0] f, 
-		output ovf
+		output reg [7:0] f, 
+		output reg ovf
 	);
-	// module-local registers
-	reg [7:0] f_reg, ovf_reg;
-	// continuous assignment to outputs from module-local registers
-	assign f = f_reg;
-	assign ovf = ovf_reg;
 	always @(a, b) begin
-		f_reg = a + b;
+		f = a + b;
 		// overflow check for addition
-		ovf_reg = (s == 0) && (f[7] != a[7]) && (f[7] != b[7]);
+		ovf = (s == 0) && (f[7] != a[7]) && (f[7] != b[7]);
 	end
 endmodule
--- lab3-report/verilog/eightbit_palu.v
+++ lab3-report/verilog/eightbit_palu.v
@@ -5,20 +5,15 @@ module eightbit_palu(
 		output [7:0] f, 
 		output ovf
 	);
-	// module-local registers
-	reg [7:0] f_reg, ovf_reg;
-	// continuous assignment to outputs from module-local registers
-	assign f = f_reg;
-	assign ovf = ovf_reg;
 	always @(a, b, s) begin
 		// operations on 'f'
 		case(s)
-			0: f_reg = a + b;
-			1: f_reg = ~b;
-			2: f_reg = a & b;
-			3: f_reg = a | b;
+			0: f = a + b;
+			1: f = ~b;
+			2: f = a & b;
+			3: f = a | b;
 		endcase
 		// overflow check for addition
-		ovf_reg = (s == 0) && (f[7] != a[7]) && (f[7] != b[7]);
+		ovf = (s == 0) && (f[7] != a[7]) && (f[7] != b[7]);
 	end
 endmodule
