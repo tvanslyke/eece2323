22b116683b1838848313d3edd98dbad3878bd67b
Timothy Van Slyke <vanslyke.t@husky.neu.edu>
2018-02-13 13:40:08 -0500

    new tb

--- lab4-verilog-code/alu_reg_tb.v
+++ lab4-verilog-code/alu_reg_tb.v
@@ -41,32 +41,6 @@ module alu_reg_tb();
 
 	integer test_duration = 150;
 	
-	// always @(posedge clk) begin
-	// 	if ($time < test_duration) begin
-	// 		$display("Clock Tick (posedge t = %d): ", $time);
-	// 		$display({"Inputs:\n",
-	// 			"rst = %b, ",
-	// 			"write enable = %b, ",
-	// 			"write data = %b, ",
-	// 			"instr_i = %b, ",
-	// 			"mux 1 select = %b, ",
-	// 			"mux 2 select = %b, ",
-	// 			"ALU op = %b, "
-	// 		}, rst, write_enable, write_data, instr_i, alu_src1, alu_src2, alu_op);
-	// 	end
-	// end
-	always @(negedge clk) begin
-		if ($time < test_duration) begin
-			$display("Clock Tick (negedge t = %d): ", $time);
-			$display({"Ouputs:\n",
-				"ALU operand 1 = %b, ",
-				"ALU operand 2 = %b, ",
-				"ALU output = %b, ",
-				"ALU ovf = %b, ",
-				"ALU take_branch = %b, "
-			}, alu_input1, alu_input2, alu_result, ovf, take_branch);
-		end
-	end
 	initial begin
 		clk = 0;
 		rst = 0;
@@ -79,73 +53,36 @@ module alu_reg_tb();
 		alu_src1 = 0;
 		alu_src2 = 0;
 		alu_op = 0;
-		#10  // load value into addr 0
-		write_address = 0;
-		write_data = 8'b1000_0000;
-		#10 // load value into addr 1
-		write_address = 1;
-		write_data = 8'b1000_0001;
-		#10 // read values into respective operands
-		write_enable = 0;
-		read_address1 = 0;
-		read_address2 = 1;
-		#10 // load into address 2 
-		write_enable = 1;
-		write_address = 2;
-		write_data = 8'b1110_0111;
-		#10 // read address 2 into operand 2
-		write_enable = 0;
-		read_address2 = 2;
-		#10 // test reset functionality
-		rst = 1;
-		#10 //load values back into registers
-		rst = 0;
-		write_enable = 1;
-		write_address = 2;
-		write_data = 8'b1110_0111;
 		#10  
-		write_address = 0;
-		write_data = 8'b1000_0000;
-		#10 // read values we just loaded into operands
+		rst = 1;
+		#10
+		// read registers
 		write_enable = 0;
-		read_address1 = 0;
-		read_address2 = 2;
+
+		#10
+		// immediate
+		instr_i = 1;
+		// enable mux to load immediate 
+		alu_src2 = 1;
+		// write to reg
+		write_data = 8'hab;
+		write_enable = 1;
+		alu_op = 0;
+		#10
+		// load immediate
+		instr_i = 8'hA0;
+		// and op
+		alu_op = 2;
 		
-		// test multiplexers
-		instr_i = 8'b1111_1111;
 		#10
-		alu_src1 = 1;
-		alu_src2 = 0;
+		// and reg value with instr_i
+		instr_i = 8'hAB;
 		#10
-		alu_src1 = 0;
-		alu_src2 = 1;
+		write_data = result; 
 		#10
-		alu_src1 = 1;
-		alu_src2 = 1;
-		#10
-		alu_src1 = 0;
-		alu_src2 = 0;
-		#10
-		alu_op = 0; // no-op
-		// test ALU opcodes
-		// #10
-		// alu_op = 1;
-		// #10
-		// alu_op = 1;
-		// #10
-		// alu_op = 2;
-		// #10
-		// alu_op = 3;
-		// #10
-		// alu_op = 4;
-		// #10
-		// alu_op = 5;
-		// #10
-		// alu_op = 6;
-		// #10
-		// alu_op = 7;
-		// #10
-		// alu_op = 7; // noop
+		alu_op = 2; // noop
+		
+	
 	end
 
 endmodule
