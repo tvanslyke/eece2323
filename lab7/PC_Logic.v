ca6a6b60ad7ca5ffc7c9e895f68c9ea75012f320
Timothy Van Slyke <vanslyke.t@husky.neu.edu>
2018-04-03 14:09:59 -0400

    fixed PC_Logic module

--- lab7/PC_Logic.v
+++ lab7/PC_Logic.v
@@ -1,10 +1,9 @@
 
-module PC_Logic(input clk, rst, output [7:0] count);
-
-	reg [7:0] internal_count;
-	always @(rst) internal_count <= 0;
-	always @(clk) internal_count <= internal_count + 1;
-	assign count = internal_count;
-
+module PC_Logic(input clk, rst, output reg [7:0] count);
+	always @(posedge clk) begin
+		if(rst) count <= 0;
+		else count <= count + 1;
+	end 
 endmodule
+
 
